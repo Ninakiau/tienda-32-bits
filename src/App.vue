73c114f4e3a40e92950eced6911456e551410f36
Claudia <claudia.ortiz@uc.cl>
2024-06-17 23:13:14 -0400

    add table and vue actions

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,77 @@
 <template>
+  <div id="app">
+    <h1>Tienda 32 Bits</h1>
+    <h3>Lista de Juegos</h3>
+    <table>
+      <thead>
+        <tr>
+          <th>Código</th>
+          <th>Nombre</th>
+          <th>Stock</th>
+          <th>Precio</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="juego in $store.state.juegos" :key="juego.codigo">
+          <td>{{ juego.codigo }}</td>
+          <td>{{ juego.nombre }}</td>
+          <td>{{ juego.stock }}</td>
+          <td>{{ juego.precio }}</td>
+          <td>
+            <button @click="incrementarStock(juego)">+</button>
+            <button @click="disminuirStock(juego)">-</button>
+          </td>
+        </tr>
+      </tbody>
 
+    </table>
+  </div>
 </template>
 
 <script>
-
+import { mapState, mapActions } from 'vuex';
 
 export default {
   name: 'App',
-  components: {
-
+  computed: {
+    ...mapState({
+      juegos: state => state.juegos
+    })
+  },
+  mounted() {
+    console.log(this.juegos);
+  },
+  methods: {
+    ...mapActions(['incrementarStock', 'disminuirStock'])
   }
 }
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 
+table {
+  width: 100%;
+}
+
+table,
+th,
+td {
+  border: 1px solid black;
+  border-style: groove;
+}
+
+th,
+td {
+  padding: .5rem;
+  text-align: center;
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,31 @@
-import { createStore } from 'vuex'
-import {juegos } from '../db/juegos.json'
+import { createStore } from "vuex";
+import juegosData from "../db/juegos.json";
 export default createStore({
   state: {
-    juegos
+    juegos: juegosData,
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    SET_STOCK(state, { codigo, stock }) {
+      const juego = state.juegos.find((juego) => juego.codigo === codigo);
+      if (juego) {
+        juego.stock = stock;
+      }
+    },
   },
+
   actions: {
+    incrementarStock({ commit }, juego) {
+      const nuevoStock = parseInt(juego.stock) + 1;
+      commit('SET_STOCK', { codigo: juego.codigo, stock: nuevoStock });
+    },
+    disminuirStock({ commit }, juego) {
+      const nuevoStock = parseInt(juego.stock) - 1;
+      if (nuevoStock >= 0) {
+        commit('SET_STOCK', { codigo: juego.codigo, stock: nuevoStock });
+      }
+    }
   },
-  modules: {
-  }
-})
+  
+  modules: {},
+});
